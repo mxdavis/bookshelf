b8e5b77cb7f55cb37bc75fb985274effb43393d7
mxdavis <md01@me.com>
2018-02-26 13:20:28 +0200

    add author model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Author.create(first_name: "Malki", last_name: "Davis", birth_year: 1985, is_alive: true)
+
+Author.create(first_name: "Sam", last_name: "Brown", birth_year: 1895, is_alive: false)
