4b55a08935b7899ed757a552668d4f87e0535f1d
mxdavis <md01@me.com>
2018-02-27 22:12:20 +0200

    create author mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,21 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
+
+  # query to call this Mutation with id and full name being returned after author created
+  # mutation {
+  #   createAuthor(first_name:"Jim", last_name: "Smith", birth_year: 1900, is_alive: false) {
+  #    id
+  #    full_name
+  #  }
+  # }
+
+  field :createAuthor, Types::AuthorType do
+    argument :first_name, types.String
+    argument :last_name, types.String
+    argument :birth_year, types.Int
+    argument :is_alive, types.Boolean
+
+    resolve ->(obj, args, ctx) { Author.create args.to_h }
   end
 end
