9cc3a32a3f310002cac36b9a0b2d5fb7576bd13a
mxdavis <md01@me.com>
2018-03-05 22:32:05 +0200

    update check auth to allow is_public  and check for session key in context

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,18 +1,14 @@
 class GraphqlController < ApplicationController
 
-  before_action do
-    unless @session = Session.where(key: request.headers['Authorization']).first
-      head(:unauthorized)
-      false
-    end
-  end
+  before_action :check_authentication
 
   def execute
     variables = ensure_hash(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      current_user: @session.user
+      current_user: @session.user,
+      session_key: @session.try(:key)
     }
     result = BookshelfSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
@@ -35,6 +31,25 @@ class GraphqlController < ApplicationController
       {}
     else
       raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
+    end
+  end
+
+  def check_authentication
+    parsed_query = GraphQL::Query.new BookshelfSchema, params[:query]
+    operation = parsed_query.selected_operation.selections.first.name
+    return true if operation == '__schema'
+
+    field = BookshelfSchema.query.fields[operation] || BookshelfSchema.mutation.fields[operation]
+    return true if field.metadata[:is_public]
+
+    unless @session = Session.where(key: request.headers['Authorization']).first
+      head(:unauthorized)
+      return false
+    end
+
+    unless field.metadata[:must_be].to_a.include? @session.user.role
+      head(:unauthorized)
+      return false
     end
   end
 end
