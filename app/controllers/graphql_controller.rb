a6bc833a2a7afcb46bd4a4fd2380d57a040a351b
mxdavis <md01@me.com>
2018-03-05 22:16:09 +0200

    check for authorization in controller before allowing any queries

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,11 +1,18 @@
 class GraphqlController < ApplicationController
+
+  before_action do
+    unless @session = Session.where(key: request.headers['Authorization']).first
+      head(:unauthorized)
+      false
+    end
+  end
+
   def execute
     variables = ensure_hash(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: @session.user
     }
     result = BookshelfSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
