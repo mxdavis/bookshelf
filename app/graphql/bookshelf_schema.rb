3e1712b4832b413fb0a57bf4a6f5377141e4378a
mxdavis <md01@me.com>
2018-03-05 22:21:41 +0200

    create is public metadata

--- app/graphql/bookshelf_schema.rb
+++ app/graphql/bookshelf_schema.rb
@@ -1,3 +1,5 @@
+GraphQL::Field.accepts_definitions is_public: GraphQL::Define.assign_metadata_key(:is_public)
+
 BookshelfSchema = GraphQL::Schema.define do
   mutation(Types::MutationType)
   query(Types::QueryType)
